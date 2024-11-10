7309e77a8b80b2d43525b317bb1d6c2c7bd3053f
sumitdevs <mysumitdevs@gmail.com>
2024-11-10 16:04:24 +0530

    dashboard data created

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,8 +4,7 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-
+import { lusitana } from '../fonts';
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
@@ -16,16 +15,15 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
